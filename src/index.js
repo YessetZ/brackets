93903c048602fb12d8f5a1dd4438c6fc88b83bed
yessetz <yessetkhan@gmail.com>
2022-07-30 13:14:24 +0500

    feat: add functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let bracketsOpen = new Array();
+  let bracketsClose = new Array();
+
+  let tempOpen = '';
+
+  for (let i in str) {
+    
+      tempOpen = str[i];
+  
+      for (let x in bracketsConfig) {
+  
+          if (bracketsConfig[x][0] === tempOpen) {
+              bracketsOpen.push(bracketsConfig[x][0]);
+              bracketsClose.push(bracketsConfig[x][1]);
+              break;
+          }
+      }
+
+      for (let j = parseInt(i) + 1; j < str.length; j++) {
+
+          if ( (bracketsClose[bracketsClose.length-1] === str[j]) && (i % 2 != j % 2) ) {
+              bracketsClose.pop();
+              bracketsOpen.pop();
+              str = str.substring(0, j) + ' ' + str.substring(j + 1);
+              break;
+          }
+
+      }
+
+  }
+
+  return bracketsOpen.length === 0 && bracketsClose.length === 0;
 }
